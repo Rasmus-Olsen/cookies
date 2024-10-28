700d187bd4899a4a2953db9f6831e43629bbe2d9
Rasmus Olsen <rasmusolsen@Rasmuss-MBP.home>
2024-10-28 21:07:10 +0100

    test

--- src/components/CookiePopup.jsx
+++ src/components/CookiePopup.jsx
@@ -1,13 +1,17 @@
-import { LuCookie } from "react-icons/lu";
+import { FaCookieBite } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 
-const CookiePopup = () => {
+export default function Home() {
   return (
-    <div className="cookie-popup">
-      <LuCookie style={{ fontSize: "2rem" }} />
-      <p>We use cookies to improve your user experience</p>
-      <button>I Like Cookies</button>
+    <div className="cookie-layout">
+      <div className="icons">
+        <FaCookieBite className="cookie-icon" />
+        <IoMdClose className="close-icon" />
+      </div>
+      <div className="cookie-text">
+        <p>We use cookies to improve your user experience.</p>
+        <button className="cookie-button">I like cookies</button>
+      </div>
     </div>
   );
-};
-
-export default CookiePopup;
+}
