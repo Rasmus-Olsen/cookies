3abb7dfccb02c7dde9e6d49eda7c877741543ced
Rasmus Olsen <rasmusolsen@Rasmuss-MacBook-Pro.local>
2024-10-29 13:10:45 +0100

    rettelser af classes

--- src/components/CookiePopup.jsx
+++ src/components/CookiePopup.jsx
@@ -3,14 +3,14 @@ import { IoMdClose } from "react-icons/io";
 
 export default function Home() {
   return (
-    <div className="cookie-layout">
-      <div className="icons">
-        <FaCookieBite className="cookie-icon" />
-        <IoMdClose className="close-icon" />
+    <div className="cookie">
+      <div className="cookie-icons">
+        <FaCookieBite size={50} className="cookie-icons-cookie" />
+        <IoMdClose size={20} className="cookie-icons-close" />
       </div>
-      <div className="cookie-text">
+      <div className="cookie-content">
         <p>We use cookies to improve your user experience.</p>
-        <button className="cookie-button">I like Cookies</button>
+        <button className="cookie-content-button">I like Cookies</button>
       </div>
     </div>
   );
