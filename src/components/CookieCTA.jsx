085dd96a9dd2deb77b96eccf8cd1bbdaeb9ebb34
Rasmus Olsen <rasmusolsen@Rasmuss-MBP.home>
2024-10-31 13:11:17 +0100

    dark mode

--- src/components/CookieCTA.jsx
+++ src/components/CookieCTA.jsx
@@ -1,5 +1,5 @@
 const CookieCTA = ({ ctatext }) => {
-  return <button className="cookie-content-button">{ctatext}</button>;
+  return <button className="cookie-content-button dark:bg-black dark:text-white  bg-white  text-black text-lg">{ctatext}</button>;
 };
 
 export default CookieCTA;
--- src/components/CookiePopup.jsx
+++ src/components/CookiePopup.jsx
@@ -5,13 +5,13 @@ import CookieCTA from "./CookieCTA";
 
 const CookiePopup = () => {
   return (
-    <div className="cookie">
-      <div className="cookie-icons">
+    <div className="cookie dark:bg-white bg-black">
+      <div className="cookie-icons dark:text-black text-white">
         <FaCookieBite size={40} className="cookie-icons-cookie" />
         <IoMdClose size={20} className="cookie-icons-close" />
       </div>
       <div className="cookie-content">
-        <p>We use cookies to improve your user experience.</p>
+        <p className="text-white dark:text-black">We use cookies to improve your user experience.</p>
         <CookieCTA ctatext="I like Cookies" />
       </div>
     </div>
